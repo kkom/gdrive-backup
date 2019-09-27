8c4eaf0c58635585b6e61e81810181fad177f2fa
Konrad Komorowski <konrad@komorowski.me>
2019-09-27 13:52:43 +0100

    specify the port

--- docker_image/gdrive-backup.bash
+++ docker_image/gdrive-backup.bash
@@ -18,4 +18,4 @@ GDRIVE_BACKUP_CMD="rclone copy \
     gcs:$STORAGE_BUCKET_NAME/backup_\$(date --utc +%Y%m%d_%H%M)"
 
 # Perform the backup on HTTP trigger
-shell2http -port 80 / "$GDRIVE_BACKUP_CMD"
+shell2http -port $PORT / "$GDRIVE_BACKUP_CMD"
